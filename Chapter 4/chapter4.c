8c2f03dcf96d638d12c32a3f9085d5b5e47aa3e5
Ruggero D'Alò <Ruggero D'Al>
2012-07-12 19:49:46 +0200

    Max Sub Array Finished

--- Chapter 4/chapter4.c
+++ Chapter 4/chapter4.c
@@ -1,60 +1,104 @@
+//Author: Ruggero D'Alò
+//Chapter 4 algorithms implementation
+
 #include "chapter4.h"
 
-max * max_crossing_sub(int *array, int l,int h){
-   int maxsumlf=0,maxsumrt=0,i=0,sum=0;
-   max * max_array=malloc(sizeof(max));
+//Divide et ImperaO(nlog(n))
+Max * max_crossing_sub(int *array,int l,int h){
+  int maxsumlf=0,maxsumrt=0,i=0,sum=0;
+  Max * max_array=malloc(sizeof(Max));
 
-   for (i=(l+h)/2;i>=l;i--){
-      sum = sum + array[i];
-      if (sum > maxsumlf){
-         maxsumlf = sum;
-         max_array->maxlf = i;
-      }
-   }
-   sum=0;
-   for (i=((l+h)/2)+1;i<h;i++){
-      sum = sum + array[i];
-      if (sum > maxsumrt){
-         maxsumrt = sum;
-         max_array->maxrt = i;
-      }
-   }
-   max_array->maxsum = maxsumlf +maxsumrt;
-   return max_array;
+  for (i=(l+h)/2;i>=l;i--){
+    sum = sum + array[i];
+    if (sum > maxsumlf){
+      maxsumlf = sum;
+      max_array->maxlf = i;
+    }
+  }
+  sum=0;
+  for (i=((l+h)/2)+1;i<h;i++){
+    sum = sum + array[i];
+    if (sum > maxsumrt){
+      maxsumrt = sum;
+      max_array->maxrt = i;
+    }
+}
+  max_array->maxsum = maxsumlf +maxsumrt;
+return max_array;
 }
 
-max * find_maximum_subarray(int *array,int l, int h){
-   int i=0;
-   max * crossing=NULL;
-   max * array_left=NULL;
-   max * array_right=NULL;
+Max * max_subarray(int *array,int l,int h){
+  int i=0;
+  Max * crossing=NULL;
+  Max * array_left=NULL;
+  Max * array_right=NULL;
 
-   if (h==l){
-      crossing = malloc(sizeof(max));
-      crossing->maxlf = l;
-      crossing->maxrt = h;
-      crossing->maxsum = array[l];
+  if (h==l){
+    crossing = malloc(sizeof(Max));
+    crossing->maxlf = l;
+    crossing->maxrt = h;
+    crossing->maxsum = array[l];
+    return crossing;
+  }
+  else {
+    array_left = max_subarray(array,l,(l+h)/2);
+    array_right = max_subarray(array,((l+h)/2)+1,h);
+    crossing = max_crossing_sub(array,l,h);
+
+    if ((array_left->maxsum > array_right->maxsum) && (array_left->maxsum > crossing->maxsum)){
+      free(array_right);
+      free(crossing);
+      return array_left;
+    }
+    else if ((array_right->maxsum > array_left->maxsum) && (array_right->maxsum > crossing->maxsum)){
+      free(array_left);
+      free(crossing);
+      return array_right;
+    }
+    else {
+      free(array_right);
+      free(array_left);
       return crossing;
-      }
-   else {
-      array_left = find_maximum_subarray(array,l,(l+h)/2);
-      array_right = find_maximum_subarray(array,((l+h)/2)+1,h);
-      crossing = max_crossing_sub(array,l,h);
-
-      if ((array_left->maxsum > array_right->maxsum) && (array_left->maxsum > crossing->maxsum)){
-         free(array_right);
-         free(crossing);
-         return array_left;
-      }
-      else if ((array_right->maxsum > array_left->maxsum) && (array_right->maxsum > crossing->maxsum)){
-         free(array_left);
-         free(crossing);
-         return array_right;
-      }
-      else {
-         free(array_right);
-         free(array_left);
-         return crossing;
-      }
-   }
+    }
+  }
 }
+//End Divide et Impera
+
+//Exercise 4.1-2 Quadratic O(n^2)
+void max_subarray_quadratic(int *array,int size,Max *result){
+  int i=0,j=0,sum=0;
+  result->maxsum = 0;
+  for (i=0;i<size;i++){
+    for (j=i;j<size;j++){
+      sum = sum + array[j];
+      if (sum>result->maxsum){
+        result->maxlf = i;
+        result->maxrt = j;
+        result->maxsum = sum;
+      }
+    }
+    sum = 0;
+  }
+return;
+}
+
+//Exercise 4.1-5 Linear O(n)
+void max_subarray_linear(int *array,int size,Max *result){
+  int i=0,j=0,sum=0;
+
+  for (i=0;i<size;i++){
+    sum = sum +array[i];
+    if (sum>result->maxsum){
+      result->maxlf = j;
+      result->maxrt = i;
+      result->maxsum = sum;
+    }
+    if (sum < 0){
+      sum =0;
+      j = i+1;
+    }
+  }
+return;
+}
+
+
--- Chapter 4/chapter4.h
+++ Chapter 4/chapter4.h
@@ -1,17 +1,22 @@
+//Author: Ruggero D'Alò
+
 #ifndef CHAPTER4_H_INCLUDED
 #define CHAPTER4_H_INCLUDED
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct maxtag
+typedef struct Maxtag
 {
-   int maxlf;
-   int maxrt;
-   int maxsum;
-}max;
+  int maxlf;
+  int maxrt;
+  int maxsum;
+}Max;
 
-max * max_crossing_sub(int *array, int l,int h);
-max * find_maximum_subarray(int *array,int l, int h);
+Max * max_crossing_sub(int *array, int l,int h);
+Max * max_subarray(int *array,int l, int h);
+void max_subarray_quadratic(int *array,int size,Max *result);
+void max_subarray_linear(int *array,int size,Max *result);
+
 
 #endif // CHAPTER4_H_INCLUDED
