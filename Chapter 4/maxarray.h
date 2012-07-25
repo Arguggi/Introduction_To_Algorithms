8918aa7b828a587007cf2ecdf364d52cb0214b65
Arguggi <Arguggi+git@gmail.com>
2012-07-25 10:33:22 +0200

    Max struct documentation

--- Chapter 4/maxarray.h
+++ Chapter 4/maxarray.h
@@ -3,6 +3,13 @@
     \brief Maximum subarray Algorithms
 */
 
+/** \struct Max maxarray.h "Chapter 4/maxarray.h"
+    \brief Struct containing 2 indexs and the sum of array[maxlf] to array[maxrt]
+    \param maxlf Left index of the maximum subarray
+    \param maxrt Right index of the maximum subarray
+    \param maxsum Sum of the elements from array[maxlf] to [maxrt]
+*/
+
 /** \fn Max * max_crossing_sub(int *array, int l,int h)
     \brief Finds maximum sum of elements crossing the (l+h)/2 index
     \param array Integer Array
@@ -25,14 +32,14 @@
     \brief Brute force solution to the max sub array problem
     \param array Integer Array
     \param size Elements in the array
-    \param *result Pointer to an already allocated structure where the result is stored 
+    \param *result Pointer to an already allocated structure where the result is stored
 */
 
 /** \fn void max_subarray_linear(int *array,int size,Max *result);
     \brief Kadane's algorithm
     \param array Integer Array
     \param size Elements in the array
-    \param *result Pointer to an already allocated structure where the result is stored 
+    \param *result Pointer to an already allocated structure where the result is stored
 */
 
 #ifndef MAXARRAY_H_INCLUDED
@@ -41,12 +48,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Maxtag /**<Struct containing 2 indexs and the sum of array[maxlf] to array[maxrt]*/
+typedef struct Maxtag
 {
-  int maxlf; /**< Left index of the maximum subarray */
-  int maxrt; /**< Right index of the maximum subarray */
-  int maxsum; /**< Sum of the elements from array[maxlf] to [maxrt] */
-}Max; /**<Name of the struct:*/
+  int maxlf;
+  int maxrt;
+  int maxsum;
+}Max;
 
 Max * max_crossing_sub(int *array, int l,int h);
 Max * max_subarray(int *array,int l, int h);
